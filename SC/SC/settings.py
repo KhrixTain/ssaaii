50126057fc47348343c66325cd9684bc91ddaaeb
Guillermo <guillermoluisc@gmail.com>
2020-08-29 15:57:42 -0300

    Actualizaciín de la base de datos, y carga de configuracion

--- SC/SC/settings.py
+++ SC/SC/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'SC.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'sa2bd',
+        'USER': 'sa2',
+        'PASSWORD': 'admin',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
