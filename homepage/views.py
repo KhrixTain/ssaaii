5c600471fbd1d649f848834d58647a05c07d39bd
Christian Maccarino <christian_maccarino@outlook.com>
2020-09-11 16:02:50 -0300

    modificación homepage para el title

--- homepage/views.py
+++ homepage/views.py
@@ -2,3 +2,6 @@ from django.views.generic import TemplateView
 
 class MyHomePage(TemplateView):
     template_name = 'homepage.html'
+    extra_context = {
+        'title': "Página Principal",
+    }
