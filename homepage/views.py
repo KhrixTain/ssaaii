012dd0e15a34c13b49564d5a6b6c63923267f9e1
Christian Maccarino <christian_maccarino@outlook.com>
2020-09-16 01:21:59 -0300

    ya ni se que hice jaja

--- homepage/views.py
+++ homepage/views.py
@@ -1,10 +1,15 @@
 from django.views.generic import TemplateView
+from django import template
+from django.contrib.auth.models import Group
+
+register = template.Library()
 
 class MyHomePage(TemplateView):
     template_name = 'index.html'
     extra_context = {
         'title': "Página Principal",
     }
+
 class LibroDiarioView(TemplateView):
     template_name = 'libro_diario_view.html'
     extra_context = {
