0797d783802e42ca9a4392fe36beb055c33cec64
Christian Maccarino <christian_maccarino@outlook.com>
2020-10-28 18:57:28 -0300

    correcciones multiples de demasiadas cosas que fallaban antes del deadline, se me esta cayendo el pelo

--- homepage/urls.py
+++ homepage/urls.py
@@ -3,9 +3,9 @@ from django.urls import path
 
 
 from homepage.views import MyHomePage, LibroMayor, CargarAsiento
-
+app_name="homepage"
 urlpatterns = [
-    path('', login_required(MyHomePage.as_view()), name="homepage"),
+    path('', login_required(MyHomePage.as_view()),name="homepage"),
     path('cargar-asiento/', login_required(CargarAsiento.as_view()), name="cargar-asiento"),
     path('libroMayor/', login_required(LibroMayor.as_view()), name="libroMayor"),
 ]
