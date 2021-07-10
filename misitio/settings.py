b5422327be3bca721e1f39e78067c54c05d35b16
Khrixtain <christian_maccarino@outlook.com>
2021-07-09 21:27:17 -0300

    Cambios menores realizados(?)

--- misitio/settings.py
+++ misitio/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 from django.conf.global_settings import LOGOUT_REDIRECT_URL, LOGIN_URL
@@ -92,7 +93,7 @@ WSGI_APPLICATION = 'misitio.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': str(BASE_DIR / "db.sqlite3"),
     }
 }
 
